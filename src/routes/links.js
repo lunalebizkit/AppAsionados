8dd8387236969366133b55a499a669cbfa663122
Ale Luna <lunalebizkit@gmail.com>
2021-08-23 21:47:57 -0300

    vida a la pagina de futbol

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,7 +1,7 @@
-const express= require('express');
-const ruta= express.Router();
-const db= require('../database');
-const {estaLogueado, noEstaLogueado, admin}=require('../lib/auth');
+const express = require('express');
+const ruta = express.Router();
+const db = require('../database');
+const { estaLogueado, noEstaLogueado, admin } = require('../lib/auth');
 
 //Agregue pantalla equipo
 ruta.get('/equipo', estaLogueado, async (req, res) => {
@@ -9,8 +9,12 @@ ruta.get('/equipo', estaLogueado, async (req, res) => {
 });
 
 //agregue pantalla futbol
-ruta.get('/futbol', estaLogueado, async (req, res) => {
-    res.render('paginas/futbol');
+ruta.get('/futbol/:id', estaLogueado, async (req, res) => {
+    const {id}= req.params;
+    const equipos= await db.query('select * from equipos inner join deporte where equipos.idDeportes = deporte.idDeportes');
+    const miEquipo= await db.query('select * from equipos where idUsuarios =?', [id]);
+    console.info(miEquipo);
+    res.render('paginas/futbol', {equipos, miEquipo});
 });
 
 //agregue pantalla basquet
@@ -32,40 +36,44 @@ ruta.get('/deporte', estaLogueado, async (req, res) => {
 ruta.get('/cancha', estaLogueado, async (req, res) => {
     res.render('paginas/cancha');
 });
-ruta.get('/vistaAdmin', estaLogueado, async(req, res) =>{
-    const cookie= req.session.cookie;
+ruta.get('/vistaAdmin', estaLogueado, async (req, res) => {
+    const cookie = req.session.cookie;
     console.info(req.user);
     console.info(cookie);
     res.render('paginas/vistaAdmin');
 });
-ruta.post('/vistaAdmin', estaLogueado, async(req, res) =>{
+ruta.post('/vistaAdmin', estaLogueado, async (req, res) => {
     console.info(req.body);
     res.render('paginas/vistaAdmin');
 });
-ruta.get('/crearEquipoFutbol/:id', async (req, res) =>{    
+ruta.get('/crearEquipoFutbol/:id', async (req, res) => {
     res.render('paginas/crearEquipoFutbol');
 });
-ruta.post('/crearEquipoFutbol/:id', async(req, res )=>{
-    const {id}= req.params;
+ruta.post('/crearEquipoFutbol/:id', async (req, res) => {
+    const { id } = req.params;
     const idUsuarios = id
-    const {nombreEquipo, idDeportes}= req.body;
-    let newEquipo= {
+    const { nombreEquipo, posicion, idDeportes } = req.body;
+    let newEquipo = {
         nombreEquipo,
         idDeportes,
         idUsuarios
+    };
+    const consulta= await db.query('Select * from equipos Where nombreEquipo =?', [nombreEquipo]);
+    if((consulta.length)>0) {
+        req.flash('mensajeMal', "Equipo Existente");
+        res.redirect('/paginas/crearEquipoFutbol/:id')
     }
-    // const crearEquipo = await db.query('Insert into equipos set ?', [newEquipo]);
-    const idEquipo= crearEquipo.insertId
-    let newJugador= {
-        idUsuarios,
-        /*posicion,*/
-        idDeportes,
-        idEquipo
-    }
-    console.info(newEquipo);
-    console.info(newJugador);   
-    //await db.query('Insert into jugador set ?', [newJugador]);
-    res.redirect('/paginas/futbol');
+    else {const crearEquipo = await db.query('Insert into equipos set ?', [newEquipo]);
+            const idEquipo = crearEquipo.insertId;
+            let newJugador= {
+            idUsuarios,
+            posicion,
+            idDeportes,
+            idEquipo
+            };
+            await db.query('Insert into jugador set ?', [newJugador]);
+            req.flash('mensajeOk', "Equipo Creado con Exito!!!");
+            res.redirect('/paginas/futbol')};    
 });
 
-module.exports= ruta;
+module.exports = ruta;

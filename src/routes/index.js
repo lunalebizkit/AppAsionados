353fdb59b4705d929d3d33ee8c7d76023594b940
Ale Luna <lunalebizkit@gmail.com>
2021-08-31 18:31:28 -0300

    logo resnponsive

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { estaLogueado, noEstaLogueado, admin, duenio } = require('../lib/auth');
 const ruta = express.Router();
-ruta.get('/', (req, res) => {
+ruta.get('/', noEstaLogueado, async(req, res) => {
    res.render('ingreso/ingreso');
 });
 module.exports= ruta;
